16a8d79c9a7e2d859440b4411cab4ef75b3f52ce
Kadievka Salcedo <kadievka.s.ks@gmail.com>
2021-08-11 16:07:19 -0400

    login page style

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faEye,
+  faEyeSlash,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -10,6 +13,23 @@ export class LoginComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  faEye = faEye
+  faEyeSlash = faEyeSlash
+
+  passwordType: string = 'password';
+  showPasswordIcon = faEye;
+
+  showPassword(): void {
+    if (this.passwordType === 'password') {
+      this.passwordType = 'text';
+      this.showPasswordIcon = this.faEyeSlash;
+    } else {
+      this.passwordType = 'password';
+      this.showPasswordIcon = this.faEye;
+    }
   }
 
 }
