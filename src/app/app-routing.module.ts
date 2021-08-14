58185ce240f0efcc40bc72eaf05fdd3b16f0dd70
Kadievka Salcedo <kadievka.s.ks@gmail.com>
2021-08-14 10:32:51 -0400

    fix navigation component and add profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { LoginGuardService } from './guards/login-guard.service';
 import { AboutComponent } from './about/about.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent, canActivate: [LoginGuardService]},
   { path: '404', component: NotFoundComponent },
   { path: 'About', component: AboutComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: '**', redirectTo: '/404' },
 ];
 
